78d3236cebb5e6a658d99cbca8d5579be2eb6391
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-04-21 23:06:13 -0300

    First tabs layout

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
+
+import { AiOutlineStar, AiFillStar, AiOutlineHome, AiFillHome } from 'react-icons/ai';
+
 import styles from '../styles/components/Tabs.module.css';
 
 export function Tabs() {
     return (
         <div className={styles.tabsContainer}>
+            <Link href="/">
+                <button>
+                    <AiOutlineHome size={32} />
+                </button>
+            </Link>
             
+            <Link href="/legends">
+                <button>
+                    <AiOutlineStar size={32} />
+                </button>
+            </Link>
         </div>
     );
 }

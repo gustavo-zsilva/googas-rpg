d66a2d4b8a5cc5943e2d4a4a4f79691fc3cf9bcf
Gustavo Zonta <gustavozontadasilva@gmail.com>
2025-09-19 09:42:04 -0300

    added glow based on rarity

--- src/components/LegendImage.tsx
+++ src/components/LegendImage.tsx
@@ -5,20 +5,32 @@ import Shimmer from 'react-shimmer-effect';
 
 import Tilt from 'react-parallax-tilt';
 
-import { rarityScheme } from '../utils/rarityScheme';
+import { rarityScheme } from '../utils/colorSchemes';
 
 import styles from '../styles/components/Image.module.css';
 
 export function LegendImage() {
     const { legend, isRevealing } = useLegends();
+
+    const rarityGlowScheme = {                          // Controls boxShadow spread based on rarity (more rare = more glow)
+        'rare': '10px',
+        'epic': '30px',
+        'legendary': '60px',
+        'mythical': '100px',
+    }
     
     return (
         <Tilt scale={1.1}>
             <div
                 className={styles.imageContainer}
-                style={{ border: `8px solid ${rarityScheme[legend?.rarity]}` }}
+                style={{
+                    border: `8px solid ${rarityScheme[legend?.rarity]}`,
+                    boxShadow: Object.keys(rarityGlowScheme).includes(legend?.rarity) ? `0 0 ${rarityGlowScheme[legend?.rarity]} ${rarityScheme[legend?.rarity]}` : 'none'
+                }}
             >
-                <div style={{ background: isRevealing ? 'transparent' : null }} />
+                <div style={{
+                    background: isRevealing ? 'transparent' : null,
+                }} />
                 
                 { legend && (
                     // <Shimmer>

3fefccb6da6cf2ff334fb1eac5697902dd8803b8
Gustavo Zonta <gustavozontadasilva@gmail.com>
2025-09-22 16:40:55 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     images: {
-        domains: ['lh3.googleusercontent.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+            }
+        ]
     }
 }
